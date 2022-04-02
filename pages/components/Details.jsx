80a81717b015fc977f1f356007118f54de702f91
Lekan Dar <Lekandar11@gmail.com>
2022-04-01 23:53:15 -1200

    version-1.7

--- pages/components/Details.jsx
+++ pages/components/Details.jsx
@@ -20,7 +20,7 @@ const Details = () => {
     <>
       <section className="block lg:flex px-4 lg:px-16 mt-24 lg:mt-64">
         <div className="font-body w-full lg:w-5/6 dark:text-white ">
-          <h1 className=" text-primaryText text-4xl md:text-6xl lg:text-7xl lg:text-[64px] font-bold  text-center lg:text-left">
+          <h1 className=" text-primaryText dark:text-white text-4xl md:text-6xl lg:text-7xl lg:text-[64px] font-bold  text-center lg:text-left">
             How <span className="text-mainGreen">FarmVest</span> Works
           </h1>
           <p className="text-sm lg:text-lg text-primaryText w-full lg:w-5/6 dark:text-white  text-center lg:text-left">
@@ -70,7 +70,16 @@ const Details = () => {
       </section>
 
       <div className="flex flex-col-reverse lg:flex-row gap-12 px-4 lg:px-12 mt-24 lg:mt-64">
-        <img src="/boy.png" alt="boy" className=" w-full lg:w-1/2" />
+        <img
+          src="/boy.png"
+          alt="boy"
+          className=" w-full lg:w-1/2 dark:hidden"
+        />
+        <img
+          src="/darkboy.png"
+          alt="boy"
+          className=" w-full lg:w-1/2 hidden dark:inline-block"
+        />
         <div className="font-body w-full lg:w-4/6 dark:text-white text-primaryText text-center lg:text-left">
           <h1 className="text-4xl md:text-6xl lg:text-[70px] font-bold mb-8">
             Your <span className="text-mainGreen"> pathway </span> to Future
@@ -81,7 +90,7 @@ const Details = () => {
             mollis aenean sit dictum tincidunt. Ut arcu, suscipit ac etLorem
             ipsum dolor sit amet.Lorem ipsum dolor sit amet, consectetur
           </p>
-          <div className="flex justify-between items-center py-4 bg-[#F2F8F2] px-4 rounded-md shadow-md">
+          <div className="flex justify-between items-center py-4 bg-[#F2F8F2] dark:bg-[#313C42] px-4 rounded-md shadow-md">
             <h1 className="text-lg">What is FarmVest?</h1>
             <span>
               <MdOutlineKeyboardArrowDown
@@ -90,7 +99,7 @@ const Details = () => {
               />
             </span>
           </div>
-          <div className="flex justify-between items-center py-4 bg-[#F2F8F2] px-4 rounded-md shadow-md my-6">
+          <div className="flex justify-between items-center py-4 bg-[#F2F8F2] dark:bg-[#313C42] px-4 rounded-md shadow-md my-6">
             <h1 className="text-lg">What is FarmVest?</h1>
             <span>
               <MdOutlineKeyboardArrowDown
@@ -99,7 +108,7 @@ const Details = () => {
               />
             </span>
           </div>
-          <div className="flex justify-between items-center py-4 bg-[#F2F8F2] px-4 rounded-md shadow-md ">
+          <div className="flex justify-between items-center py-4 bg-[#F2F8F2] dark:bg-[#313C42] px-4 rounded-md shadow-md ">
             <h1 className="text-lg">What is FarmVest?</h1>
             <span>
               <MdOutlineKeyboardArrowDown
