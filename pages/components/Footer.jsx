f8aafa6ffde4c1bc8b10fec4c0bb8c37c2dabd73
Lekan Dar <Lekandar11@gmail.com>
2022-04-02 20:09:45 -1200

    version-2.1

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -3,22 +3,22 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="mt-44 bg-[#F2F8F2] dark:bg-[#354046] py-12 px-8">
-        <section className="flex">
-          <div>
-            <h1>FarmVest</h1>
-            <p>
+      <div className="mt-44 bg-[#F2F8F2] dark:bg-[#354046] dark:text-white pt-12 px-8 text-center lg:text-left ">
+        <section className="block lg:flex justify-between gap-28 border-b-2 pb-8 border-[#2632384F]">
+          <div className="pb-4">
+            <h1 className="text-xl">FarmVest</h1>
+            <p className="text-lg">
               High level experience in web design and development knowledge,
               producing quality work.
             </p>
           </div>
           {/* 2nd Flex */}
-          <div>
-            <p>
+          <div className="pb-4">
+            <p className="text-lg">
               Subscribe to stay tuned for new web design and latest updates.
               Let&rsquo;s do it!
             </p>
-            <div className="md:bg-[#E9EBEB] md:w-auto py-2 md:py-4 px-8 md:px-2 rounded-md">
+            <div className="md:bg-[#E9EBEB] py-3 md:py-1 px-8 md:px-2 mt-2 rounded-md flex justify-between">
               <input
                 type="text"
                 placeholder="Enter your email"
@@ -26,22 +26,53 @@ const Footer = () => {
               />
               <a
                 href="#"
-                className="bg-mainGreen  block md:inline py-3 px-6 cursor-pointer text-white rounded-lg shadow-md mt-2 md:mt-0"
+                className="bg-mainGreen hover:bg-green-700 block md:inline py-2 lg:py-3 px-8 cursor-pointer text-white rounded-lg shadow-md"
               >
-                Get Started
+                Subscribe
               </a>
             </div>
           </div>
           {/* 3rd Flex */}
-          <div>
+          <div className="text-lg  pb-4">
             <h1>Follow us</h1>
+            <div className="flex justify-center my-4 lg:justify-between gap-2 md:gap-8">
+              <img src="/ig.png" alt="icon" className="dark:hidden" />
+              <img src="/facebook.png" alt="icon" className="dark:hidden" />
+              <img src="/twitter.png" alt="icon" className="dark:hidden" />
+              <img
+                src="/darkig.png"
+                alt="icon"
+                className="hidden dark:inline-block"
+              />
+              <img
+                src="/darkfacebook.png"
+                alt="icon"
+                className="hidden dark:inline-block"
+              />
+              <img
+                src="/darktwitter.png"
+                alt="icon"
+                className="hidden dark:inline-block"
+              />
+            </div>
           </div>
           {/* 4th Flex */}
-          <div>
+          <div className="text-lg  pb-4">
             <h1>Call us</h1>
             <span>+2349123456789</span>
           </div>
         </section>
+
+        <article className="block lg:flex justify-between text-primaryText dark:text-white pt-4 text-base">
+          <p>© 2021 All Rights Reserved</p>
+          <ul className="flex gap-8">
+            <li>Privacy Policy</li>
+            <li>Terms of Use</li>
+            <li>Sales and Refunds</li>
+            <li>Legal</li>
+            <li>Site Map</li>
+          </ul>
+        </article>
       </div>
     </>
   );
